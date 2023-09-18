6e35a75e123987bad0c48fe67e3ae43ea91eab46
Jesus Pech <jpech4503@gmail.com>
2023-09-17 23:58:09 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const RoutesTwilio = require('./routes/twilio-sms-app');
+var cors = require("cors");
+
+
+const port = process.env.PORT || "3000";
+
+app.listen(port,()=>console.log(`server on port ${port}`));
+
+//middleware
+app.use(cors());
+app.use(express.json())
+app.use('/', RoutesTwilio);
